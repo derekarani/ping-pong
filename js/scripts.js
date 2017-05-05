ee48f19e8a9cc02540fbbd986d363eb697b73ce7
derekarani <derekarani@gmail.com>
2017-05-05 14:52:00 +0300

    Code works just fine.Upnext the styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,37 @@
-$(document).ready(function() {
   // Business logic begins here
   var ping = "Ping"
   var pong = "Pong"
   var pingpong = "Ping Pong"
   var oldArray = [];
-  var range = _.range(1, pp + 1);
-  var newArray = oldArray.push(range)
-  var method = oldArray.forEach(function() {
-    if ( % 3 === 0) {
-      return ping;
-    } else if ( % 5 === 0) {
-      return pong;
-    } else if ( % 15 === 0) {
-      return pingpong;
-    } else {
-      return
+  var game = function(number) {
+
+    for (var i = 1; i <= number; i++) {
+      if ((i % 3 === 0) && (i % 5 === 0)) {
+        oldArray.push(pingpong);
+      } else if (i % 3 === 0) {
+        oldArray.push(ping)
+      } else if (i % 5 === 0) {
+        oldArray.push(pong)
+      } else {
+        oldArray.push(i);
+      }
     }
+    return oldArray;
+  }
+
+  // user interface code begins here
+  $(document).ready(function() {
+    $("form#pong").submit(function(event) {
+      event.preventDefault();
+      
+      var number = parseInt($("input#ping").val());
+      var output =game(number)
+
+
+      oldArray.forEach(function(number){
+
+        $("ul.result").append("<li>" +number+"</li>")
+      });
+
+    });
   });
-
-  
-
-});
-
-
-
-
-
-
-
-
-
-// user interface code begins here
-$("form#pong").submit(function(event) {
-event.preventDefault();
-var pp = parseInt($("input#ping").val());
-});
-});
